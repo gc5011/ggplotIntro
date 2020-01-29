303039f518258bf292275b1b4b25ea763223018d
Geoff <geoff.chan@burnet.edu.au>
2020-01-29 11:46:53 +1100

    Add setup script for plot session

---
+++ setup.R
@@ -0,0 +1,13 @@
+# Load libraries
+
+library(
+    gapminder
+    , ggplot2
+    , ggthemes
+    , dplyr
+    , tidyr
+    , RColorBrewer
+)
+
+# Assign gapminder data into data frame
+data <- gapminder
